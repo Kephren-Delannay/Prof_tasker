86634bb2f401ea7e75ce0472f7b74d3cdc4ce45c
Kephren <kephren.delannay.sampany@gmail.com>
2022-06-23 13:58:49 -0400

    Adjusted the constants

--- New_take.py
+++ New_take.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-# dummy classes
-CLASSES = ['2ndGT1', '2ndGT2', '2ndGT3', '2ndGT4', 'TG1', 'TG2', 'TG3', 'TG4']
+# region CONST
+# classes
+CLASSES = ['2GT1', '2GT2', '2GT3', '2GT4', '2GT5', '2GT6', '2GT7', '2GT8', '2GT9', '2GT10', '2GT11', '2nd_Options', '2nd_Autres',
+           '1eGT1', '1eGT2', '1eGT3', '1eGT4', '1eGT5', '1eGT6', '1eGT7', '1eGT8', '1eGT9', '1ere_Spécialirés', '1ere_Autres', '1ere_ST2S', '1ere_STMG',
+           'TG1', 'TG2', 'TG3', 'TG4', 'TG5', 'TG6', 'TG7', 'TG8', 'Term_Spécialités', 'Term_Autres', 'TST2S', 'TSTMG',
+           'BTS1_COM', 'BTS2_COM', 'BTS1_MCO', 'BTS2_MCO', 'BTS1_SAM', 'BTS2_SAM', 'BTS1_CG', 'BTS2_CG', 'ECG1', 'ECG2', 'AL1', 'AL2']
 
-# dummy subjects
-SUBJECTS = ['Maths', 'SVT', 'Philosophie', 'Anglais']
+# subjects
+SUBJECTS = ['L0100_PHILOSOPHIE', 'L0201_LETT CLASS', 'L0202_LETT MOD', 'L0421_ALLEMAND', 'L0422_ANGLAIS', 'L0426_ESPAGNOL', 'L0449_CREOLE', 'L0433_PORTUGAIS',
+            'L1000_HIT GEO', 'L1100_SC ECO SOC', 'L1300_MATH', 'L1500_PHYS CHIM', 'NSI',
+            'L1600_SVT', 'L1800_ARTS PLASTIQUES', 'L6500_ENS ART & ARTS APP', 'L7100_BIOC GENIE BIOL', 'L7300_SC & TECH MEDICO', 'L8012_ECO GEST COMP FIN',
+            'L8013_ECO GEST CM', 'L8014_COMMUNICATION']
+
+# endregion
 
 
 def create_classes_df(_classes, _subjects):
@@ -14,19 +23,19 @@ def create_classes_df(_classes, _subjects):
     return _data
 
 
-def change_classes_per_subject(classe, subject, value):
+def change_classes_per_subject(_classe, _subject, value):
     global subjects_df
-    subjects_df.at[classe, subject] = value
+    subjects_df.at[_classe, _subject] = value
 
 
 def create_profs_df():
-    data = {'Total_Hours' : [], 'Assigned_Hours': [], 'Sup' : [], 'Subject' : []}
+    data = {'Total_Hours': [], 'Assigned_Hours': [], 'Sup': [], 'Subject': []}
     return data
 
 
-def add_new_prof( name, hours, sup, subject):
+def add_new_prof( name, hours, sup, _subject):
     global profs_df
-    profs_df.loc[name] = [hours, 0, sup, subject]
+    profs_df.loc[name] = [hours, 0, sup, _subject]
 
 
 def initialize_assignment():
@@ -38,13 +47,14 @@ def initialize_assignment():
     return df
 
 
-def add_prof_to_subject(subject, classes, prof):
-    subject.loc[prof] = [0 for x in range(len(classes))]
+def add_prof_to_subject(_subject, prof):
+    global CLASSES
+    _subject.loc[prof] = [0 for x in range(len(CLASSES))]
 
 
-def assign_hour( subject, classe, prof, hours):
+def assign_hour(_subject, classe, prof, hours):
     global profs_df
-    subject.at[prof, classe] = hours
+    _subject.at[prof, classe] = hours
     profs_df.at[prof, 'Assigned_Hours'] += hours
 
 
@@ -60,13 +70,13 @@ subject = initialize_assignment()
 # dummy manipulation
 
 # affect a class to a subject
-change_classes_per_subject('2ndGT2', 'Philosophie', 3)
+change_classes_per_subject('2GT2', 'L0100_PHILOSOPHIE', 3)
 
 # create a new prof
-add_new_prof('Jean', 10, 3, 'SVT')
+add_new_prof('Jean', 10, 3, 'L0100_PHILOSOPHIE')
 
 # assign a prof to a class
-add_prof_to_subject(subject, CLASSES, 'Jean')
-assign_hour(subject, '2ndGT2', 'Jean', 4)
+add_prof_to_subject(subject, 'Jean')
+assign_hour(subject, '2GT2', 'Jean', 4)
 
-print(profs_df)
+print(subject)
